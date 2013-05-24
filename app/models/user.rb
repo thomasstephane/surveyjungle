c0514eea0ecf5398342490ce0ed25214c3fa6c58
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-24 12:18:56 -0700

    Updated user model with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :surveys
+  has_many :responses
+  has_many :participations
+
+  validates :username, presence: true
+  validates :email, presence: true
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
