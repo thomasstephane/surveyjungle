9fc0bf900241c8f0fbba7913569ef4e3b394b1cc
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 18:59:40 -0700

    js adding new questions tdd: adding new choices

--- public/js/application.js
+++ public/js/application.js
@@ -1,26 +1,14 @@
-$(document).ready(function() {
 
-  $('form[name="signin"]').on('submit', function(e) {
-    e.preventDefault();
-    console.log(this);
-    $.ajax({
-      type: 'post',
-      url: '/signin',
-      data: $(this).serialize()
-    }).done(function(data) {
-      console.log(data);
-    });
-  });
+$(document).ready(function(){
 
-  $('form[name="signup"]').on('submit', function(e) {
-    e.preventDefault();
-    console.log($(this).serialize());
-    $.ajax({
-      type: 'post',
-      url: '/signup',
-      data: $(this).serialize()
-    }).done(function(data) {
-      console.log(data);
+  var addDiv = $('#addinput');
+
+  var i = $('#addinput p').size() + 1;
+
+  $('#addNew').on('click', function() {
+    $('<p><input id="question" type="text" name="p_new_' + i +'" value="" placeholder="I am New" /><a href="#" id="remNew">Remove</a> </p>').appendTo(addDiv);
+    i++;
+
+    
     });
-  });
 });
