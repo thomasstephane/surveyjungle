239d115f895b2fd0c08a6c6ec557d28304468a97
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-25 13:19:28 -0700

    switching to master to pull Stephane js

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
   var addDiv = $('.addinput');
   var i = $('.addinput p').size() - 1;
+  if (i > 0) {
+
+
   $('.addQuestion').on('click', function() {
-    $('<div class="question_section"><span class="question_add"><p><li><input class="question" type="text" name="question' + i +'" placeholder="Question" /><a href="#" class="remNew">Remove</a> </li><p></span><span class="choice_add"><p><input class="choice" type="text" name="choice" placeholder="Choice"><a href="#" class="addChoice">Add Choice</a></p></span></div>').appendTo(addDiv);
+    $('<div class="question_section"><span class="question_add"><p><li><input class="question" type="text" name="question' + i +'" placeholder="Question" /><a href="#" class="remNew">Remove</a> </li></p></span><span class="choice_add"><p><input class="choice" type="text" name="choice" placeholder="Choice"><a href="#" class="addChoice">Add Choice</a></p></span></div>').appendTo(addDiv);
     i++;
   return false;
 });
+    }
 
 $(document).on("click", ".remNew", function(event){
 event.preventDefault();
@@ -23,7 +27,7 @@ return false;
 
 $(document).ready(function(){
   var addChoice = $('.choice_add');
-  var i = $('.choice_add p').size();
+  var i = $(this).closest('span').children('p').size();
   $('form').on('click', '.addChoice',function() {
     $('<p><input class="choice" type="text" name="choice' + i +'" placeholder="Choice"><a href="#" class="remChoice">Remove</a></p>').appendTo($(this).parents('p'));
     i++;
@@ -41,3 +45,5 @@ i--;
 return false;
 });
 });
+
+
