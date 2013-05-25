d1cdc4fe3e655da8e278ad8ee8116f4441f9d737
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-25 01:36:40 -0700

    squashed bug in js code

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   var addDiv = $('.addinput');
-  var i = $('.addinput p').size() + 1;
+  var i = $('.addinput p').size() - 1;
   $('.addQuestion').on('click', function() {
-    $('<div class="question_section"><span class="question_add"><p><li><input class="question" type="text" name="question_new_' + i +'" placeholder="Question" /><a href="#" class="remNew">Remove</a> </li><p></span><span class="choice_add"><p><input class="choice" type="text" name="choice_new" placeholder="Choice"><a href="#" class="addChoice">Add Choice</a></p></span></div>').appendTo(addDiv);
+    $('<div class="question_section"><span class="question_add"><p><li><input class="question" type="text" name="question' + i +'" placeholder="Question" /><a href="#" class="remNew">Remove</a> </li><p></span><span class="choice_add"><p><input class="choice" type="text" name="choice" placeholder="Choice"><a href="#" class="addChoice">Add Choice</a></p></span></div>').appendTo(addDiv);
     i++;
   return false;
 });
@@ -23,9 +23,9 @@ return false;
 
 $(document).ready(function(){
   var addChoice = $('.choice_add');
-  var i = $('.choice_add p').size() + 1;
+  var i = $('.choice_add p').size();
   $('form').on('click', '.addChoice',function() {
-    $('<p><input class="choice" type="text" name="choice_new_' + i +'" placeholder="Choice"><a href="#" class="remChoice">Remove</a></p>').appendTo($(this).parents('p'));
+    $('<p><input class="choice" type="text" name="choice' + i +'" placeholder="Choice"><a href="#" class="remChoice">Remove</a></p>').appendTo($(this).parents('p'));
     i++;
   return false;
 });
@@ -34,7 +34,7 @@ $(document).on("click", ".remChoice", function(event){
 event.preventDefault();
     console.log(this);
 if( i > 2 ) {
-$(this).parents('p').remove();
+$(this).closest('p').remove();
 console.log(this);
 i--;
 }
