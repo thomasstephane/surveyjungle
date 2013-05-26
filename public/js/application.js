e2da16ed4e24d022d19b1e213545c717ccee2da4
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-25 17:00:06 -0700

    js and controllers done

--- public/js/application.js
+++ public/js/application.js
@@ -1,47 +1,48 @@
 $(document).ready(function(){
   var addDiv = $('.addinput');
-  var i = $('.addinput p').size() - 1;
-  if (i > 0) {
+  // var i = $('.addinput p').size();
+  // if (i > 0) {
 
 
   $('.addQuestion').on('click', function() {
-    $('<div class="question_section"><span class="question_add"><p><li><input class="question" type="text" name="question' + i +'" placeholder="Question" /><a href="#" class="remNew">Remove</a> </li></p></span><span class="choice_add"><p><input class="choice" type="text" name="choice" placeholder="Choice"><a href="#" class="addChoice">Add Choice</a></p></span></div>').appendTo(addDiv);
+    $('<div class="question_section"><span class="question_add"><p><li><input class="question" type="text" name="questions[][question]" placeholder="Question" /><a href="#" class="remNew">Remove</a> </li></p></span><span class="choice_add"><p><input class="choice" type="text" name="questions[][choices][]" placeholder="Choice"><a href="#" class="addChoice">Add Choice</a></p></span></div>').appendTo(addDiv);
     i++;
   return false;
 });
-    }
+    // }
 
 $(document).on("click", ".remNew", function(event){
 event.preventDefault();
     console.log(this);
-if( i > 2 ) {
+// if( i > 2 ) {
 
 $(this).parents('.question_section').remove();
 
 console.log(this);
-i--;
-}
+// i--;
+// }
 return false;
 });
 });
 
 $(document).ready(function(){
   var addChoice = $('.choice_add');
-  var i = $(this).closest('span').children('p').size();
+  // var i = $(this).closest('span').children('p').size();
+
   $('form').on('click', '.addChoice',function() {
-    $('<p><input class="choice" type="text" name="choice' + i +'" placeholder="Choice"><a href="#" class="remChoice">Remove</a></p>').appendTo($(this).parents('p'));
-    i++;
+    $('<p><input class="choice" type="text" name="questions[][choices][]" placeholder="Choice"><a href="#" class="remChoice">Remove</a></p>').appendTo($(this).parents('p'));
+    // i++;
   return false;
 });
 
 $(document).on("click", ".remChoice", function(event){
 event.preventDefault();
     console.log(this);
-if( i > 2 ) {
+// if( i > 0 ) {
 $(this).closest('p').remove();
 console.log(this);
-i--;
-}
+// i--;
+// }
 return false;
 });
 });
