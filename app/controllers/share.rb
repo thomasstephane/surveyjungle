5f152b322c8d2ad451c7c27c056ea3c0d3386702
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-27 00:49:25 -0700

    Added the content of the email send for sharing in app/controllers/share.rb
    Body, message and link to survey added.

--- app/controllers/share.rb
+++ app/controllers/share.rb
@@ -12,19 +12,20 @@ end
 
 post '/share/:survey_id' do |survey_id|
   @user = current_user
+  @survey = Survey.find(survey_id)  
   params["survey"]["emails"].each do |email|
     invity = User.find_by_email(email)
     if invity
       Participation.create(invited: "invited", survey_id: survey_id, user_id: invity.id)
     else
       Invitation.create(invited_email: email, survey_id: survey_id, user_id: @user.id)
-      send_mail(email)
+      send_mail(email, @user, @survey)
     end
   end
   redirect '/'
 end
 
-def send_mail(email)
+def send_mail(email, user, survey)
   Pony.mail({
     :to => email,
     :via => :smtp,
@@ -36,6 +37,9 @@ def send_mail(email)
       :password             => ENV["password"],
       :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
       :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-    }
+    },
+    :from => user.email,
+    :subject => "Would you like to answer the survey: #{survey.title}",
+    :body => "Hello!\n Check in at Survey Jungle to complete this survey: #{survey.title}.\n\nMore about this survey:\n#{survey.description}. \n\n\nCheck out http\:\/\/surveyjungle.herokuapp.com"
   })
 end
