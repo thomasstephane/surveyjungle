35c53603d028695e0044177d6776c3609297dd1b
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-28 14:48:50 -0700

    Change style to make buttons and inputs more visible
    modified:   public/css/analyze.css
    modified:   public/css/answer.css
    modified:   public/css/application.css
    modified:   public/css/create.css
    modified:   public/css/normalize.css
    modified:   public/css/share.css
    modified:   public/js/share.js

--- public/js/share.js
+++ public/js/share.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
   var addMail = $('.section-share');
 
 
-  $('.add-email').on('click', function() {
+  $('.add-email').on('click', function(e) {
+    e.preventDefault();
     var newMail = $('<div class="new-mail"><img src="../images/share.jpg"><input class="email-share" type="text" name="survey[emails][]"  placeholder="Email"><a href="#" class="remMail">Remove</a></div>').hide();
     newMail.appendTo(addMail).slideDown("slow");
   });
