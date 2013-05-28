7d56d4ad1136807ff39a4ea5e9abf2a3f7d3f334
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-28 01:21:54 -0700

    Fix mail format missing and password min length for user signup app/controllers/user.rb
    Corrected display for case where no stats available, put "-" and size blue zone 0% app/views/survey_display.erb
    
    Corrected the various case of blank field in the survey form to avoid bullshit data  app/controllers/index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,19 +11,31 @@ post '/survey' do
     title: params[:title],
     description: params[:description],
     user_id: session[:user_id], open: open)
-
   @questions = params[:questions]
-  @questions.each do |question|
-    @question_value = question["question"]
-    @question = Question.create(question: @question_value)
-    @choices = question["choices"]
-    @choices.each do |choice|
-      @question.choices.create(choice: choice)
+  p @questions
+  if @survey.title != "" && @questions != [{"question"=>"", "choices"=>[""]}]
+    @questions.each do |question|
+      @question_value = question["question"]
+      @question = Question.create(question: @question_value) if @question_value != "" && @question_value != nil
+      @choices = question["choices"]
+      @choices.each do |choice|
+        @question.choices.create(choice: choice)  if choice != "" && choice != nil
+      end
+      if @question.choices.length == 0
+        @question.destroy
+      else
+        @survey.questions << @question
+      end
     end
-    @survey.questions << @question
-  end
-
-  redirect "/share/#{@survey.id}"
+    if @survey.questions.length != 0
+      redirect "/share/#{@survey.id}"
+    else
+      @survey.destroy
+      redirect "/"
+    end
+else
+  redirect "/"
+end
 end
 
 get '/survey' do
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,10 +1,29 @@
 post '/signup' do 
   content_type :json
   if User.find_by_username(params[:username])
+    puts "hi"
     {msg: "Username already taken"}.to_json
   elsif User.find_by_email(params[:email])
+    puts "hi"
+    puts "hi"
     {msg: "Email already taken"}.to_json
+  elsif !(params[:email] =~ /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/)
+    puts "hi"
+    puts "hi"
+    puts "hi"
+    {msg: "Doesn't look like an email"}.to_json
+  elsif params[:password].length < 8
+    puts "hi"
+    puts "hi"
+    puts "hi"
+    puts "hi"
+    {msg: "Password to short, 8 min"}.to_json
   else
+    puts "hi"
+    puts "hi"
+    puts "hi"
+    puts "hi"
+    puts "hi"
     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
     Invitation.add_participations(@user)
     session[:user_id] = @user.id
